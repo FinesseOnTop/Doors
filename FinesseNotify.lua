c38ad44cc09c09f6c56ce4603f4727756173430a
Finesse <93530654+FinesseOnTop@users.noreply.github.com>
2023-02-04 19:25:38 +0700

    Update FinesseNotify.lua

--- FinesseNotify.lua
+++ FinesseNotify.lua
@@ -2,7 +2,7 @@ workspace.ChildAdded:Connect(function(Obj)
     if Obj.Name:sub(1, 1) == "A" and Obj.Name ~= "AmbushMoving" then
         game.StarterGui:SetCore("SendNotification", {
             Title = Obj.Name .. " Spawned ⚠️",
-            Text = "Hide quick",
+            Text = "Tron di",
             Duration = 5
         })
     end
@@ -11,7 +11,7 @@ workspace.ChildRemoved:Connect(function(Obj)
     if Obj.Name:sub(1, 1) == "A" and Obj.Name ~= "AmbushMoving" then
         game.StarterGui:SetCore("SendNotification", {
             Title = Obj.Name .. " DeSpawned ✅",
-            Text = "ur good",
+            Text = "ok roi",
             Duration = 5
         })
     end
